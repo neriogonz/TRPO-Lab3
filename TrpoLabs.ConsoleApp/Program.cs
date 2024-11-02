3142376c193eb9f451c658b4753703cc528fcb97
neriogonz <147056665+neriogonz@users.noreply.github.com>
2024-11-02 20:49:55 +0500

    correction

---
+++ TrpoLabs.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TrpoLabs.Core;
+
+namespace TrpoLabs.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2, 3);
+            Console.WriteLine($"El resultado de 2 + 3 es: {result}");
+        }
+    }
+}
