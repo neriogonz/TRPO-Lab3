9a2d11f13f424210dfb374b5e6476478f526ac81
neriogonz <147056665+neriogonz@users.noreply.github.com>
2024-11-04 15:38:45 +0500

    ff

---
+++ TrpoLabs.Core/TrpoLabs.WebApp/Controllers/CalculateAreaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrpoLabs.WebApp.Models;
+using TrpoLabs.Core;
+
+namespace TrpoLabs.WebApp.Controllers
+{
+    public class CalculateAreaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CalculateAreaViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculateAreaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Calculator calculator = new Calculator();
+                model.Resultado = calculator.CalcularAreaCono(model.Radio, model.Altura);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ TrpoLabs.Core/TrpoLabs.WebApp/Models/CalculateAreaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrpoLabs.WebApp.Models
+{
+    public class CalculateAreaViewModel
+    {
+        [Required(ErrorMessage = "Введите радиус.")]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Радиус должен быть больше нуля.")]
+        public double Radio { get; set; }
+
+        [Required(ErrorMessage = "Введите высоту.")]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Высота должна быть больше нуля.")]
+        public double Altura { get; set; }
+
+        public double Resultado { get; set; }
+    }
+}
